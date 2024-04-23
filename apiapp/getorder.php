f50b5f726ff7c1cfc13b3e2b448dfc3a45c7bd87
Công Tuyền ✓ <94374796+ctuyendeveloper@users.noreply.github.com>
2024-04-23 12:27:56 +0700

    asd

--- apiapp/getorder.php
+++ apiapp/getorder.php
@@ -14,8 +14,8 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once './connection.php';
 
 try {
-    $sqlQuery = "SELECT orderdetail.orderdetail_id, orderdetail.orderdetail_quantity, orderdetail.orderdetail_price, bill.bill_createdat, bill.bill_status, bill.bill_note, bill.bill_paymentmethod, address.address_detail, address.address_phone, address.address_name, product.product_name, product.product_price FROM orderdetail 
-    INNER JOIN bill ON bill.bill_id = orderdetail.bill_id INNER JOIN address ON bill.address_id = address.address_id INNER JOIN product ON orderdetail.product_id = product.product_id ";
+    $sqlQuery = "SELECT bill.bill_id, orderdetail.orderdetail_id, orderdetail.orderdetail_quantity, orderdetail.orderdetail_price, bill.bill_createdat, bill.bill_status, bill.bill_note, bill.bill_paymentmethod, address.address_detail, address.address_phone, address.address_name, product.product_name, product.product_price FROM bill 
+    INNER JOIN orderdetail ON bill.bill_id = orderdetail.bill_id INNER JOIN address ON bill.address_id = address.address_id INNER JOIN product ON orderdetail.product_id = product.product_id ";
 
     $stmt = $dbConn->prepare($sqlQuery);
     $stmt->execute();
